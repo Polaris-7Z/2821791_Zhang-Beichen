efc62f763fbf2866511a1aab0de9c5d1e21aff6c
Z <128313472+Polaris-7Z@users.noreply.github.com>
2024-02-07 22:01:06 +0000

    Add files via upload

---
+++ Homework_2.py
@@ -0,0 +1,6 @@
+# Write a program that computes the area of a rectangle from the length and width.
+l = float(input("Give me a length: "))
+
+w = float(input("Give me a width: "))
+
+print("The area of the rectangle is:", l * w)
