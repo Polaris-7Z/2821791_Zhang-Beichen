d698903749f2dd3b9e1c678476b03140d45b7eb2
Z <128313472+Polaris-7Z@users.noreply.github.com>
2024-02-09 14:22:37 +0000

    Update Homework_3.py

--- Homework_3.py
+++ Homework_3.py
@@ -1,3 +1,4 @@
+# Write a program that asks you HOW MANY FRIENDS you have and then HOW MANY SWEETS YOU HAVE to share with your friends.
 f = int(input("How many friends do you have?"))
 s = int(input("How many sweets do you have?"))
 
