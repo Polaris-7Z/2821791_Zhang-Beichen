f49fad28119e67ad119af41f873650680acb8a4a
Z <128313472+Polaris-7Z@users.noreply.github.com>
2024-02-09 14:19:50 +0000

    Add files via upload

---
+++ Homework_3.py
@@ -0,0 +1,6 @@
+f = int(input("How many friends do you have?"))
+s = int(input("How many sweets do you have?"))
+
+print('You should give', s // f , "sweet(s) to each friend and then keep" , s % f , "for yourself.")
+
+quit()
