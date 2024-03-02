100131618b9f992c03d3b6c8399930f5a7f7a1a0
Z <128313472+Polaris-7Z@users.noreply.github.com>
2024-03-02 19:51:15 +0000

    Homework_10.py

--- Homework_10.py
+++ Homework_10.py
@@ -1,6 +1,6 @@
 # Output of factorial
 def F(x, a):  # Calculate the factorial
-    if a == 1 or a == 0:  # If a = 1, give back the result
+    if a == 1 or a == 0:  # If a == 1 or a == 0, give back the result
         return x
     else:
         a -= 1  # reduce a by 1
