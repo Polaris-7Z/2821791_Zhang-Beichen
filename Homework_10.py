7ba88b07518b3c7fff7355a9c42a92487f0b2a23
Z <128313472+Polaris-7Z@users.noreply.github.com>
2024-03-02 19:59:07 +0000

    Homework_10.py

--- Homework_10.py
+++ Homework_10.py
@@ -1,6 +1,6 @@
 # Output of factorial
 def F(x, a):  # Calculate the factorial
-    if a == 1 or a == 0:  # If a == 1 or a == 0, give back the result
+    if a == 1:  # If a = 1, give back the result
         return x
     else:
         a -= 1  # reduce a by 1
@@ -10,6 +10,13 @@ def F(x, a):  # Calculate the factorial
 
 # input
 n = int(input("Give me a x:"))
+
+# F(0)？
+if n == 0:
+    print("F(" + str(n) + ")=1")
+    quit()
 # print
 print("F(" + str(n) + ")=" + str(n), end="")
 print("=" + str(F(n, n)))
+
+quit()
