5f0bb547fe8704162eb57e5cabd3c5b1f8b7b86f
Z <128313472+Polaris-7Z@users.noreply.github.com>
2024-04-13 19:47:47 +0100

    Homework_14.py

--- Homework_14.py
+++ Homework_14.py
@@ -1,10 +1,12 @@
+# A program to calculate the rootfactorial
+
 import math
 def rootfactorial(x):
-    if not type(x) == int:
+    if not type(x) == int:  # Check that x is an int and raises an error if it is not (Type Error)
         raise TypeError("input is not an integer")
-    if x < 0:
+    if x < 0:  # Check that x is positive and raises an error if it is not (Value Error)
         raise ValueError("x is not positive")
-    if x > 20:
+    if x > 20:  # Check that x must be less than 20 (Value Error)
         raise ValueError("x is not less than 20")
 
     return math.sqrt(math.factorial(x))
