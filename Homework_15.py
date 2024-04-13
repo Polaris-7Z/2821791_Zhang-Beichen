8010221b39bc250e265112c9f85639e5bf89b987
Z <128313472+Polaris-7Z@users.noreply.github.com>
2024-04-13 20:25:41 +0100

    Add files via upload

---
+++ Homework_15.py
@@ -0,0 +1,19 @@
+# Asks for integers and calculates the median
+
+numbers = []  # Create a list
+
+while True:  # Asks for integers
+    x = input("Number: ")
+    if x == "done":
+        break
+    else:
+        numbers.append(int(x))  # Add the number(integer) into the list
+
+length = int(len(numbers) / 2)  # Calculate the length of the list
+
+if len(numbers) % 2 == 0:  # The even list
+    print("Median: ",  (numbers[length-1] + numbers[length]) / 2)
+else:  # The odd list
+    print("Median: ", numbers[length])
+
+quit()
